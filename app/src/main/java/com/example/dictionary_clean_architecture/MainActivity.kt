b4ba016549cb90e0c5e9c1b142cf8f214102ef30
Prince <banerjee.prince69@gmail.com>
2024-09-24 20:35:51 +0530

    WIP

--- app/src/main/java/com/example/dictionary_clean_architecture/MainActivity.kt
+++ app/src/main/java/com/example/dictionary_clean_architecture/MainActivity.kt
@@ -11,35 +11,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dictionary_clean_architecture.ui.theme.Dictionary_Clean_ArchitectureTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Dictionary_Clean_ArchitectureTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Dictionary_Clean_ArchitectureTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/dictionary_clean_architecture/feature_dictionary/data/local/WordInfoDatabase.kt
+++ app/src/main/java/com/example/dictionary_clean_architecture/feature_dictionary/data/local/WordInfoDatabase.kt
@@ -1,6 +1,7 @@
 package com.example.dictionary_clean_architecture.feature_dictionary.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.dictionary_clean_architecture.feature_dictionary.data.local.entity.WordInfoEntity
 
@@ -9,6 +10,6 @@ import com.example.dictionary_clean_architecture.feature_dictionary.data.local.e
     version = 1
 )
 @TypeConverters(Converters::class)
-abstract class WordInfoDatabase {
+abstract class WordInfoDatabase: RoomDatabase() {
     abstract val dao: WordInfoDao
 }
