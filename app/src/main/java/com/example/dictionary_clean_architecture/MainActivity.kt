eca289479e4c87de858613add27497921a5ad362
Prince <banerjee.prince69@gmail.com>
2024-09-24 21:19:31 +0530

    Final Commit

--- app/src/main/java/com/example/dictionary_clean_architecture/MainActivity.kt
+++ app/src/main/java/com/example/dictionary_clean_architecture/MainActivity.kt
@@ -1,25 +1,105 @@
 package com.example.dictionary_clean_architecture
 
+
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.dictionary_clean_architecture.feature_dictionary.presentation.WordInfoItem
+import com.example.dictionary_clean_architecture.feature_dictionary.presentation.WordInfoViewModel
 import com.example.dictionary_clean_architecture.ui.theme.Dictionary_Clean_ArchitectureTheme
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+
+import androidx.compose.material.*
+import androidx.compose.ui.Alignment
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Dictionary_Clean_ArchitectureTheme {
 
+                val viewModel: WordInfoViewModel = hiltViewModel()
+                val state = viewModel.state.value
+                val scaffoldState = rememberScaffoldState()
+
+                LaunchedEffect(key1 = true) {
+                    viewModel.eventFlow.collectLatest { event ->
+                        when (event) {
+                            is WordInfoViewModel.UIEvent.ShowSnackbar -> {
+                                scaffoldState.snackbarHostState.showSnackbar(
+                                    message = event.message
+                                )
+                            }
+                        }
+                    }
+                }
+                Scaffold(
+                    scaffoldState = scaffoldState
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .background(MaterialTheme.colors.background)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        ) {
+                            TextField(
+                                value = viewModel.searchQuery.value,
+                                onValueChange = viewModel::onSearch,
+                                modifier = Modifier.fillMaxWidth(),
+                                placeholder = {
+                                    Text(text = "Search...")
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            LazyColumn(
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                items(state.wordInfoItems.size) { i ->
+                                    val wordInfo = state.wordInfoItems[i]
+                                    if (i > 0) {
+                                        Spacer(modifier = Modifier.height(8.dp))
+                                    }
+                                    WordInfoItem(wordInfo = wordInfo)
+                                    if (i < state.wordInfoItems.size - 1) {
+                                        Divider()
+                                    }
+                                }
+                            }
+                        }
+                        if (state.isLoading) {
+                            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                        }
+                    }
+                }
             }
         }
     }
